0f8a5a9245f5874ad227441ddc356614418899ba
Anshita Bajpai <anshitapsit@gmail.com>
2025-08-24 01:03:23 +0530

    General improvements

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,13 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-export const LOGO_RED = "/ngpt-red-300x71-01.png";
+// ../utils/constants.js
+export const LOGO_RED = process.env.PUBLIC_URL + "/ngpt-red-300x71-01.png";
+export const POSTER_BG = process.env.PUBLIC_URL + "/poster-bg.jpeg";
+export const NO_POSTER = process.env.PUBLIC_URL + "/no_movie_poster.png";
+export const BACKDROP = process.env.PUBLIC_URL + "/blur.png";
+export const AVATAR_RED = process.env.PUBLIC_URL + "/avatar-red.jpeg";
+export const AVATAR_BLUE = process.env.PUBLIC_URL + "/avatar-blue.jpeg";
 
-export const POSTER_BG = "/poster-bg.jpeg";
-export const NO_POSTER = "/no_movie_poster.png";
-export const BACKDROP = "/blur.png";
-export const AVATAR_RED = "/avatar-red.jpeg";
-export const AVATAR_BLUE = "/avatar-blue.jpeg";
 
 
 
